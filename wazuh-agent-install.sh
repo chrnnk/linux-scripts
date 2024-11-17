cda589517137805ce3a865e18a4705c98feb1665
Nick Nelson <nick@chrnnk.tv>
2024-11-17 13:41:15 -0500

    Create wazuh-agent-install.sh

---
+++ wazuh-agent-install.sh
@@ -0,0 +1,5 @@
+apt install lsb-release -y
+wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.8.1-1_amd64.deb && WAZUH_MANAGER='wazuh.home.chrnnk.tv' dpkg -i ./wazuh-agent_4.8.1-1_amd64.deb
+systemctl daemon-reload
+systemctl enable wazuh-agent
+systemctl start wazuh-agent
