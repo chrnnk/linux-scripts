6be81dcceecf0dd92a71d5326fffa236dbc6ed01
Nick Nelson <nick@chrnnk.tv>
2024-11-17 13:42:49 -0500

    Update wazuh-agent-install.sh

--- wazuh-agent-install.sh
+++ wazuh-agent-install.sh
@@ -3,3 +3,8 @@ wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.8.
 systemctl daemon-reload
 systemctl enable wazuh-agent
 systemctl start wazuh-agent
+# nano /var/ossec/etc/ossec.conf
+# <ossec_config>
+#   <client>
+#     <server>
+#       <address>wazuh.home.chrnnk.tv</address>
