ab484ca9d4ddb9b62da3eff0f53b06f98c335678
Nick Nelson <nick@chrnnk.tv>
2024-11-18 12:23:32 -0500

    Update wazuh-agent-install.sh
    update to 4.9.2.1

--- wazuh-agent-install.sh
+++ wazuh-agent-install.sh
@@ -1,5 +1,5 @@
 apt install lsb-release -y
-wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.8.1-1_amd64.deb && WAZUH_MANAGER='wazuh.home.chrnnk.tv' dpkg -i ./wazuh-agent_4.8.1-1_amd64.deb
+wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.9.2-1_amd64.deb && sudo WAZUH_MANAGER='wazuh.home.chrnnk.tv' dpkg -i ./wazuh-agent_4.9.2-1_amd64.deb
 systemctl daemon-reload
 systemctl enable wazuh-agent
 systemctl start wazuh-agent
